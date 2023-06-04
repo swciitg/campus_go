aa60c6143cf7fde55e14d5de228e2a66610514b0
Chanchal Yadav <84243956+chanchalyadav272@users.noreply.github.com>
2023-06-04 23:55:05 +0530

    fonts

---
+++ build/app/intermediates/assets/debug/flutter_assets/lib/globals/my_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kWhite = Color.fromRGBO(255, 255, 255, 1);
+const Color kBlack = Color.fromRGBO(0, 0, 0, 1);
+const Color kGreen = Color.fromRGBO(58, 167, 109, 1);
+const Color kRed = Color.fromRGBO(212, 67, 51, 1);
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:campus_go/globals/my_colors.dart';
+import 'package:campus_go/globals/my_fonts.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,10 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: Center(child: Text("CampusGO")),
+      home: Center(child: Text("CampusGO", style: MyFonts.w500.setColor(kWhite).size(48),)),
     );
   }
 }
