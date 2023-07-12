27f6652777b30f8a661e2f499fef96c9c495a076
chanchalyadav272 <chanchalyadav272@gmail.com>
2023-07-12 14:55:08 +0530

    admin menu tab

--- lib/pages/outlet/menu_tab.dart
+++ lib/pages/outlet/menu_tab.dart
@@ -3,6 +3,7 @@ import 'package:campus_go/globals/my_colors.dart';
 import 'package:campus_go/globals/my_fonts.dart';
 import 'package:campus_go/models/item_model.dart';
 import 'package:campus_go/models/outlet_model.dart';
+import 'package:campus_go/pages/outlet/edit_item_page.dart';
 import 'package:campus_go/stores/cart_store.dart';
 import 'package:campus_go/stores/user_store.dart';
 import 'package:campus_go/widgets/outlet/menu_tile.dart';
@@ -23,8 +24,9 @@ class _MenuTabState extends State<MenuTab> {
   Widget build(BuildContext context) {
     var cartStore = context.read<CartStore>();
     var userStore = context.read<UserStore>();
+    ViewType viewType = userStore.viewType;
     return Observer(
-      builder: (context)=>Padding(
+      builder: (context) => Padding(
         padding: const EdgeInsets.all(16),
         child: SingleChildScrollView(
           child: Column(children: [
@@ -48,28 +50,56 @@ class _MenuTabState extends State<MenuTab> {
                               width: 8,
                             ),
                             Text(
-                              "Explore Menu",
+                              viewType == ViewType.admin
+                                  ? "Outlet Menu"
+                                  : "Explore Menu",
                               style: MyFonts.w600.setColor(kWhite).size(18),
                             )
                           ],
                         ),
                         Text(
-                          "Click \"ADD\" for adding to cart",
+                          viewType == ViewType.admin
+                              ? "Click \"Edit\" for editing info"
+                              : "Click \"ADD\" for adding to cart",
                           style: MyFonts.w300.setColor(kWhite).size(10),
                         ),
                       ]),
-                if(userStore.viewType==ViewType.user)  Container(
-                    alignment: Alignment.centerRight,
-                    child: RichText(text: TextSpan(children: [
-                      TextSpan(text: "${cartStore.totalItems} ",
-                      style: MyFonts.w500.setColor(kWhite).size(12),),
-                      TextSpan(text: cartStore.totalItems==1?"Item" :"Items",
-                      style: MyFonts.w500.setColor(kWhite).size(12),),
-                      TextSpan(text: " Added",
-                      style: MyFonts.w500.setColor(kWhite).size(12),),
-
-                    ]),)
-                  )
+                  if (viewType == ViewType.user)
+                    Container(
+                        alignment: Alignment.centerRight,
+                        child: RichText(
+                          text: TextSpan(children: [
+                            TextSpan(
+                              text: "${cartStore.totalItems} ",
+                              style: MyFonts.w500.setColor(kWhite).size(12),
+                            ),
+                            TextSpan(
+                              text:
+                                  cartStore.totalItems == 1 ? "Item" : "Items",
+                              style: MyFonts.w500.setColor(kWhite).size(12),
+                            ),
+                            TextSpan(
+                              text: " Added",
+                              style: MyFonts.w500.setColor(kWhite).size(12),
+                            ),
+                          ]),
+                        )),
+                  if (viewType == ViewType.admin)
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>const EditItemPage()));
+                      },
+                      child: Container(
+                        // height: 32,
+                        // width: 32,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8), color: lBlue),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16,vertical: 8),
+                          child: Text("Add Item", style: MyFonts.w500.setColor(kButtonText).size(14,)),
+                        )
+                      ),
+                    )
                 ],
               ),
             ),
@@ -83,7 +113,7 @@ class _MenuTabState extends State<MenuTab> {
                   price: "160",
                   category: "NON-VEG",
                   offeringOutlet: "offeringOutlet"),
-                  isOpen: widget.outletModel.status,
+              isOpen: widget.outletModel.status,
             ),
             MenuTile(
               itemModel: ItemModel(
@@ -92,7 +122,7 @@ class _MenuTabState extends State<MenuTab> {
                   price: "160",
                   category: "VEG",
                   offeringOutlet: "offeringOutlet"),
-                  isOpen: widget.outletModel.status,
+              isOpen: widget.outletModel.status,
             ),
           ]),
         ),
